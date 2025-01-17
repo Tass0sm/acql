776fa8f818dc0f76fbc575386e715c1cd423912e
Tassos Manganaris <tassos.manganaris@gmail.com>
2025-01-17 01:09:07 -0500

    Find the error to avoid exploding critic loss
    which is using min for double cost critic. should use max

--- task_aware_skill_composition/brax/agents/hdcqn_automaton_her/losses.py
+++ task_aware_skill_composition/brax/agents/hdcqn_automaton_her/losses.py
@@ -69,7 +69,7 @@ def make_losses(
 
     # Q(s_t+1, o_t+1) for all options
     next_qs = jnp.min(next_double_qs, axis=-1)
-    next_cqs = jnp.min(next_double_cqs, axis=-1)
+    next_cqs = jnp.max(next_double_cqs, axis=-1)
 
     # V(s_t+1) = max_o Q(s_t+1, o) (because pi is argmax Q)
     next_v, _ = safe_greedy_policy(next_qs, next_cqs)
@@ -121,7 +121,7 @@ def make_losses(
 
     # Q(s_t+1, o_t+1) for all options
     next_qs = jnp.min(next_double_qs, axis=-1)
-    next_cqs = jnp.min(next_double_cqs, axis=-1)
+    next_cqs = jnp.max(next_double_cqs, axis=-1)
 
     # V(s_t+1) = max_o Q(s_t+1, o) (because pi is argmax Q)
     _, next_cv = safe_greedy_policy(next_qs, next_cqs)
