07ddd3a3d303ac0607ac839b8cfba231e757596f
Tassos Manganaris <tassos.manganaris@gmail.com>
2025-04-04 13:01:00 -0400

    Fix main file

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import functools
 
 import jax
 
-from achql.brax.agents.hdcqn_automaton_her import train as hdcqn_automaton_her
+from achql.brax.agents.achql import train as achql
 from achql.brax.tasks import get_task
 from achql.brax.utils import make_aut_goal_cmdp
 
@@ -19,11 +19,11 @@ extras = {
     "options": options,
     "specification": spec,
     "state_var": task.obs_var,
-    "eval_env": make_aut_goal_cmdp(task, randomize_goals=False, margin=0.0),
+    "eval_environment": make_aut_goal_cmdp(task, randomize_goals=False, margin=0.0),
 }
 
 seed = 0
-train_fn = functools.partial(hdcqn_automaton_her.train, **task.hdcqn_her_hps)
+train_fn = functools.partial(achql.train, **task.hdcqn_her_hps)
 
 make_inference_fn, params, _ = train_fn(
     environment=make_aut_goal_cmdp(task),
