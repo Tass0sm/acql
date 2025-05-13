08583f1ec42b0ea8d209f5b4aa97aa595d27b4ca
Tassos Manganaris <tassos.manganaris@gmail.com>
2025-05-13 19:37:01 -0400

    Add to ablation script and support sum_cost_critic

--- achql/brax/agents/achql/argmaxes.py
+++ achql/brax/agents/achql/argmaxes.py
@@ -13,20 +13,21 @@ def argmax_with_random_tiebreak(array, key, axis=-1):
     return jnp.argmax(perturbed_array, axis=axis)
 
 def argmax_with_safest_tiebreak(array, safety_or_cost, axis=-1, use_sum_cost_critic=False):
+    """Note this is typically given an array where all unsafe actions are set to
+    have reward = -999"""
     max_value = jnp.max(array, axis=axis, keepdims=True)
     max_mask = (array == max_value)
     max_masked_array = jnp.where(max_mask, array, -jnp.inf)
 
     if use_sum_cost_critic:
-      raise NotImplementedError()
-      # TODO: Use axis kwarg
-      cost = safety_or_cost
-      cost_ranks = jnp.argsort(jnp.argsort(cost))
-      cost_ordered_max_array = max_masked_array - cost_ranks
-      return jnp.argmax(cost_ordered_max_array, axis=axis)
+        # TODO: Use axis kwarg?
+        cost = safety_or_cost
+        cost_ranks = jnp.argsort(jnp.argsort(cost))
+        cost_ordered_max_array = max_masked_array - cost_ranks
+        return jnp.argmax(cost_ordered_max_array, axis=axis)
     else:
-      # TODO: Use axis kwarg
-      safety = safety_or_cost
-      safety_ranks = jnp.argsort(jnp.argsort(safety))
-      safety_ordered_max_array = max_masked_array + safety_ranks
-      return jnp.argmax(safety_ordered_max_array, axis=axis)
+        # TODO: Use axis kwarg?
+        safety = safety_or_cost
+        safety_ranks = jnp.argsort(jnp.argsort(safety))
+        safety_ordered_max_array = max_masked_array + safety_ranks
+        return jnp.argmax(safety_ordered_max_array, axis=axis)
--- achql/scripts/ablation_train.py
+++ achql/scripts/ablation_train.py
@@ -1,51 +1,14 @@
-import functools
-import jax
-import os
-from typing import Optional
-
-from datetime import datetime
-from jax import numpy as jnp
-import matplotlib.pyplot as plt
-
 import mlflow
 
-import brax
+from achql.brax.agents.achql import train as achql
 
-import flax
-from brax import envs
-from brax.io import model
-from brax.io import json
-from brax.io import html
-from jaxgcrl.src import train as crl
-
-from achql.brax.agents.ppo import train as ppo
-from achql.brax.agents.tasc import train as tasc
-from achql.brax.agents.dscrl import train as dscrl
-from achql.brax.agents.sac_her import train as sac_her
-from achql.brax.agents.ppo_with_spec_rewards import train as ppo_spec
-from achql.brax.agents.ppo_option_critic import train as ppo_option_critic
-from achql.brax.agents.hsac import train as soac
-from achql.brax.agents.hdqn import train as hdqn
-from achql.brax.agents.hdqn_her import train as hdqn_her
-from achql.brax.agents.hdqn_automaton_her import train as hdqn_automaton_her
-from achql.brax.agents.hdcqn_her import train as hdcqn_her
-from achql.brax.agents.hdcqn_automaton_her import train as hdcqn_automaton_her
-from brax.training.agents.apg import train as apg
+from achql.scripts.train import train_for_all, training_run
 
 # tasks
-from achql.brax.tasks import get_task
 from achql.brax.utils import (
-    make_cmdp, make_aut_goal_cmdp, make_shaped_reward_mdp, make_shaped_reward_mdp2,
-    make_aut_mdp,
-    make_reward_machine_mdp,
-    make_aut_goal_mdp,
+    make_aut_goal_cmdp,
 )
 
-from achql.visualization.critic import (
-    make_plots_for_hdqn,
-    make_plots_for_hdcqn,
-    make_plots_for_hdcqn_aut,
-)
 
 
 def achql_ablation_one_train(run, task, seed, spec, margin=1.0):
@@ -53,10 +16,10 @@ def achql_ablation_one_train(run, task, seed, spec, margin=1.0):
 
     return training_run(
         run.info.run_id,
-        make_aut_goal_cmdp(task, margin=margin, randomize_goals=task.achql_hps["use_her"]),
+        make_aut_goal_cmdp(task, margin=margin, randomize_goals=False),
         seed,
         train_fn=achql.train,
-        hyperparameters=task.achql_hps,
+        hyperparameters=task.achql_hps | { "use_her": False },
         extras={
             "options": options,
             "specification": spec,
@@ -66,15 +29,15 @@ def achql_ablation_one_train(run, task, seed, spec, margin=1.0):
     )
 
 
-def achql_ablation_two_train(run, task, seed, spec, margin=1.0):
+def achql_ablation_two_train(run, task, seed, spec, margin=1.0, safety_threshold=None):
     options = task.get_options()
 
     return training_run(
         run.info.run_id,
-        make_aut_goal_cmdp(task, margin=margin, randomize_goals=False, relu_cost=True),
+        make_aut_goal_cmdp(task, margin=margin, randomize_goals=task.achql_hps["use_her"], relu_cost=True),
         seed,
         train_fn=achql.train,
-        hyperparameters=task.achql_hps | { "use_her": False },
+        hyperparameters=task.achql_hps | { "safety_threshold": safety_threshold },
         extras={
             "options": options,
             "specification": spec,
@@ -92,41 +55,54 @@ def all_achql_ablation_one_runs(max_seed=5):
     train_for_all(["SimpleMaze"], ["Until2"], achql_ablation_one_train, "ACHQL_ABLATION_ONE", seed_range=(0, max_seed))
     train_for_all(["SimpleMaze"], ["LoopWithObs"], achql_ablation_one_train, "ACHQL_ABLATION_ONE", seed_range=(0, max_seed))
 
-    train_for_all(["SimpleMaze3D"], ["TwoSubgoals"], achql_ablation_one_train, "ACHQL_ABLATION_ONE", seed_range=(0, max_seed))
-    train_for_all(["SimpleMaze3D"], ["Branching1"], achql_ablation_one_train, "ACHQL_ABLATION_ONE", seed_range=(0, max_seed))
-    train_for_all(["SimpleMaze3D"], ["ObligationConstraint2"], achql_ablation_one_train, "ACHQL_ABLATION_ONE", seed_range=(0, max_seed))
-    train_for_all(["SimpleMaze3D"], ["Until2"], achql_ablation_one_train, "ACHQL_ABLATION_ONE", seed_range=(0, max_seed))
-    train_for_all(["SimpleMaze3D"], ["LoopWithObs"], achql_ablation_one_train, "ACHQL_ABLATION_ONE", seed_range=(0, max_seed))
+    # train_for_all(["SimpleMaze3D"], ["TwoSubgoals"], achql_ablation_one_train, "ACHQL_ABLATION_ONE", seed_range=(0, max_seed))
+    # train_for_all(["SimpleMaze3D"], ["Branching1"], achql_ablation_one_train, "ACHQL_ABLATION_ONE", seed_range=(0, max_seed))
+    # train_for_all(["SimpleMaze3D"], ["ObligationConstraint2"], achql_ablation_one_train, "ACHQL_ABLATION_ONE", seed_range=(0, max_seed))
+    # train_for_all(["SimpleMaze3D"], ["Until2"], achql_ablation_one_train, "ACHQL_ABLATION_ONE", seed_range=(0, max_seed))
+    # train_for_all(["SimpleMaze3D"], ["LoopWithObs"], achql_ablation_one_train, "ACHQL_ABLATION_ONE", seed_range=(0, max_seed))
 
-    train_for_all(["AntMaze"], ["TwoSubgoals"], achql_ablation_one_train, "ACHQL_ABLATION_ONE", seed_range=(0, max_seed))
-    train_for_all(["AntMaze"], ["Branching1"], achql_ablation_one_train, "ACHQL_ABLATION_ONE", seed_range=(0, max_seed))
-    train_for_all(["AntMaze"], ["ObligationConstraint3"], achql_ablation_one_train, "ACHQL_ABLATION_ONE", seed_range=(0, max_seed))
-    train_for_all(["AntMaze"], ["Until2"], achql_ablation_one_train, "ACHQL_ABLATION_ONE", seed_range=(0, max_seed))
-    train_for_all(["AntMaze"], ["LoopWithObs"], achql_ablation_one_train, "ACHQL_ABLATION_ONE", seed_range=(0, max_seed))
+    # train_for_all(["AntMaze"], ["TwoSubgoals"], achql_ablation_one_train, "ACHQL_ABLATION_ONE", seed_range=(0, max_seed))
+    # train_for_all(["AntMaze"], ["Branching1"], achql_ablation_one_train, "ACHQL_ABLATION_ONE", seed_range=(0, max_seed))
+    # train_for_all(["AntMaze"], ["ObligationConstraint3"], achql_ablation_one_train, "ACHQL_ABLATION_ONE", seed_range=(0, max_seed))
+    # train_for_all(["AntMaze"], ["Until2"], achql_ablation_one_train, "ACHQL_ABLATION_ONE", seed_range=(0, max_seed))
+    # train_for_all(["AntMaze"], ["LoopWithObs"], achql_ablation_one_train, "ACHQL_ABLATION_ONE", seed_range=(0, max_seed))
 
 
 def all_achql_ablation_two_runs(max_seed=5):
-    train_for_all(["SimpleMaze"], ["ObligationConstraint1"], achql_ablation_two_train, "ACHQL_ABLATION_TWO", seed_range=(0, max_seed))
-    train_for_all(["SimpleMaze"], ["Until2"], achql_ablation_two_train, "ACHQL_ABLATION_TWO", seed_range=(0, max_seed))
-    train_for_all(["SimpleMaze"], ["LoopWithObs"], achql_ablation_two_train, "ACHQL_ABLATION_TWO", seed_range=(0, max_seed))
 
-    train_for_all(["SimpleMaze3D"], ["ObligationConstraint2"], achql_ablation_two_train, "ACHQL_ABLATION_TWO", seed_range=(0, max_seed))
-    train_for_all(["SimpleMaze3D"], ["Until2"], achql_ablation_two_train, "ACHQL_ABLATION_TWO", seed_range=(0, max_seed))
-    train_for_all(["SimpleMaze3D"], ["LoopWithObs"], achql_ablation_two_train, "ACHQL_ABLATION_TWO", seed_range=(0, max_seed))
+    safety_threshold_l = [0, 10, 40, 60]
 
-    train_for_all(["AntMaze"], ["ObligationConstraint3"], achql_ablation_two_train, "ACHQL_ABLATION_TWO", seed_range=(0, max_seed))
-    train_for_all(["AntMaze"], ["Until2"], achql_ablation_two_train, "ACHQL_ABLATION_TWO", seed_range=(0, max_seed))
-    train_for_all(["AntMaze"], ["LoopWithObs"], achql_ablation_two_train, "ACHQL_ABLATION_TWO", seed_range=(0, max_seed))
+    for safety_threshold in safety_threshold_l:
+        # train_for_all(["SimpleMaze"], ["ObligationConstraint1"], achql_ablation_two_train, "ACHQL_ABLATION_TWO", seed_range=(0, max_seed),
+        #               safety_threshold=safety_threshold)
+        # train_for_all(["SimpleMaze"], ["Until2"], achql_ablation_two_train, "ACHQL_ABLATION_TWO", seed_range=(0, max_seed),
+        #               safety_limit=safety_limit)
+        # train_for_all(["SimpleMaze"], ["LoopWithObs"], achql_ablation_two_train, "ACHQL_ABLATION_TWO", seed_range=(0, max_seed),
+        #               safety_limit=safety_limit)
 
+        # train_for_all(["SimpleMaze3D"], ["ObligationConstraint2"], achql_ablation_two_train, "ACHQL_ABLATION_TWO", seed_range=(0, max_seed),
+        #               safety_threshold=safety_threshold)
+        # train_for_all(["SimpleMaze3D"], ["Until2"], achql_ablation_two_train, "ACHQL_ABLATION_TWO", seed_range=(0, max_seed),
+        #               safety_threshold=safety_threshold)
+        # train_for_all(["SimpleMaze3D"], ["LoopWithObs"], achql_ablation_two_train, "ACHQL_ABLATION_TWO", seed_range=(0, max_seed),
+        #               safety_threshold=safety_threshold)
+
+        # train_for_all(["AntMaze"], ["ObligationConstraint3"], achql_ablation_two_train, "ACHQL_ABLATION_TWO", seed_range=(0, max_seed),
+        #               safety_threshold=safety_threshold)
+        # train_for_all(["AntMaze"], ["Until2"], achql_ablation_two_train, "ACHQL_ABLATION_TWO", seed_range=(0, max_seed),
+        #               safety_threshold=safety_threshold)
+        # train_for_all(["AntMaze"], ["LoopWithObs"], achql_ablation_two_train, "ACHQL_ABLATION_TWO", seed_range=(0, max_seed),
+        #               safety_threshold=safety_threshold)
 
 
 def main():
-    all_achql_runs()
+    # all_achql_ablation_one_runs()
+    all_achql_ablation_two_runs()
 
 
 if __name__ == "__main__":
     mlflow.set_tracking_uri("file:///home/tassos/.local/share/mlflow")
     # mlflow.set_experiment("proj2-batch-training")
-    mlflow.set_experiment("proj2-ablation-experiments")
+    mlflow.set_experiment("proj2-ablation-two-sweep")
 
     main()
